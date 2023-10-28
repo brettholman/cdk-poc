e00f6675f11fd5a2389aed4a7a8679b55aef7626
Brett Holman <brettholman91@gmail.com>
2023-10-27 17:05:48 -0700

    Add message and queue connection

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os as __os
 __env = 'dev'
 __aws_region = __os.getenv('CDK_DEFAULT_REGION')
 __account_number = __os.getenv('CDK_DEFAULT_ACCOUNT')
-dev = dict(
-    sqs_name="{}-{}-{}-healthCheckQueue".format(
-        __env, __aws_region, __account_number)
-)
+dev = {
+    "healthCheck_queue_url": "https://sqs.{}.amazonaws.com/{}/{}-{}-{}-healthCheckQueue".format(
+        __aws_region, __account_number, __env, __aws_region, __account_number)
+}
--- src/handlers/healthCheckLambda.py
+++ src/handlers/healthCheckLambda.py
@@ -1,26 +1,39 @@
-import json
 import config
 import boto3
 
 
-def handler(event, context):
+def handler(event, context) -> dict:
     response = {'code': 200}
     try:
         __handle_queue(response)
     except:
-        response.code = 500
+        response['code'] = 500
     return response
 
 
 def __handle_queue(response) -> None:
-
     try:
         response['sqs'] = {}
         sqs_client = boto3.client('sqs')
 
+        message = sqs_client.send_message(
+            QueueUrl=config.dev.healthCheck_queue_url,
+            DelaySeconds=.25,
+            MessageAttributes={
+                'body': "I came from the health check lambda"
+            },
+            MessageBody="This is a test!"
+        )
+
+        if "messageId" in message:
+            response['sqs'] = {
+                "body": "message sent!",
+                "status": "healthy"
+            }
+
     except Exception as e:
         response['sqs'] = {
             'message': "unable to send sqs message",
-            "reason": e
+            "reason": str(e)
         }
         raise e
